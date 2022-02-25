08aef2a03b4ad6396bea494f3af8d3f642e3726c
SimonVentura <vsimone474@gmail.com>
2022-02-25 14:46:14 +0100

    inziato a fare con il raw.

--- app/src/main/java/com/example/umlventura/gestore.java
+++ app/src/main/java/com/example/umlventura/gestore.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.res.Resources;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -37,5 +39,25 @@ public class gestore {
         }
 
         return sb.toString();
+    }
+
+    public String scriviFile(String nomeFile, Context c){
+        FileOutputStream fileO;
+        String testodascrivere="";
+        String esito="";
+
+        try {
+            fileO = c.openFileOutput(nomeFile,Context.MODE_PRIVATE);
+            fileO.write(testodascrivere.getBytes());
+            fileO.close();
+            esito="scrittura corretta";
+        } catch (FileNotFoundException e) { //FNF
+            e.printStackTrace();
+            esito="attenzione errore in apertura";
+        } catch (IOException e) {          //I/O
+            esito="errore in scrittura";
+        }
+
+        return esito;
     }
 }
